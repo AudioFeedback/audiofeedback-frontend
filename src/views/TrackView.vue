23de9a92baa29d29582b6fc1e92658df7687e065
Kiran Kreuze <kirank@delta-n.nl>
2023-11-30 10:16:00 +0100

    fix

--- src/views/TrackView.vue
+++ src/views/TrackView.vue
@@ -6,6 +6,7 @@ import { AVWaveform } from "vue-audio-visual";
 import { useRoute } from "vue-router";
 import { initFlowbite } from "flowbite";
 import { getRoles } from "@/utils/authorisationhelper";
+import { getProfile } from "@/services/app.service";
 
 const route = useRoute();
 const componentKey = ref(0);
@@ -18,6 +19,7 @@ const showModal = ref<boolean>(false);
 const description = ref<string>(""); 
 const audiofile = ref<File | null>(null);
 const ShowOverlay = ref<any>();
+const userinfo = ref<Components.Schemas.GetUserDto>();
 
 const trackVersion = ref<number>(0);
 
@@ -102,10 +104,10 @@ const Showoverlay = (reviewer: any) => {
     reviewerinfo.value = reviewer;
 };
 
-
 onMounted(() => {
     getTrackInfo();
     initFlowbite();
+    getUserInfo();
     window.addEventListener("resize", forceRerender);
 });
 
@@ -143,6 +145,12 @@ const seek = (seconds: number) => {
 const changeVersion = (version: number) => {
     trackVersion.value = version - 1;
     activeTab.value = 1;
+};
+
+const getUserInfo = async () => {
+    const response = await getProfile();
+
+    userinfo.value = response.data;
 };
 </script>
 
@@ -459,10 +467,16 @@ const changeVersion = (version: number) => {
                 <ol class="mt-3 divide-y divider-gray-200 dark:divide-gray-700">
                     <li>
                         <a href="#" class="items-center block p-3 sm:flex hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <img class="w-12 h-12 mb-3 me-3 rounded-full sm:mb-0" src="./../assets//profile-picture-5.jpg" alt="Jese Leos image"/>
-                            <div class="text-gray-600 dark:text-gray-400">
-                                <div class="text-base font-normal">Thomas Lean uploaded track version {{ track.versionNumber }}</div>
-                                <div class="text-sm font-normal">{{ track.description }}</div>
+                            <div class="relative inline-flex me-3 items-center justify-center w-12 h-12 overflow-hidden bg-primary-600 rounded-full dark:bg-primary-600">
+                                <span class="font-medium text-gray-300 dark:text-gray-300"
+                                    >{{ userinfo?.firstname.slice(0, 1) }}{{ userinfo?.lastname.slice(0, 1) }}</span
+                                >
+                            </div>
+                            <div class="text-gray-600 w-full dark:text-gray-400 flex flex-row items-center justify-between">
+                                <div>
+                                    <div class="text-base font-normal">Thomas Lean uploaded track version {{ track.versionNumber }}</div>
+                                    <div class="text-sm font-normal">{{ track.description }}</div>
+                                </div>
                                 <button @click="changeVersion(track.versionNumber)" type="button" class="text-white bg-blue-700 mt-2 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     View track
                                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -474,13 +488,15 @@ const changeVersion = (version: number) => {
                     </li>
                     <li  v-if='track.feedback.length >= 1'>
                         <a href="#" class="items-center block p-3 sm:flex hover:bg-gray-100 dark:hover:bg-gray-700">
-                            <img class="w-12 h-12 mb-3 me-3 rounded-full sm:mb-0" src="./../assets//profile-picture-5.jpg" alt="Bonnie Green image"/>
+                            <div class="relative inline-flex me-3 items-center justify-center w-12 h-12 overflow-hidden bg-primary-600 rounded-full dark:bg-primary-600">
+                                <span class="font-medium text-gray-300 dark:text-gray-300">?</span>
+                            </div>
                             <div>
                                 <div class="text-base font-normal text-gray-600 dark:text-gray-400">Feedback has been submitted</div>
                                 <span class="inline-flex items-center text-xs font-normal text-gray-500 dark:text-gray-400">
                                     <svg class="w-2.5 h-2.5 me-2 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                                    </svg>
+                                    </svg> 
                                     Kindly view the feedback and submit an new version
                                 </span> 
                             </div>
