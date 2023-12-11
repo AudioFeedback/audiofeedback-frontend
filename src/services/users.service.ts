feddd1185eb9255df4643714558732832312a799
Niek <frankenstein795@gmail.com>
2023-12-11 13:21:06 +0100

    Add brokey stuff back but commented

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -13,6 +13,18 @@ export async function createUser(body: Paths.UsersControllerCreate.RequestBody) 
     return client.UsersController_create(null, body);
 }
 
+// export async function updateUser(body: Paths.UsersControllerUpdate.RequestBody, id: number) {
+//     const client = await APIClient();
+//
+//     return client.UsersController_update(id, body);
+// }
+//
+// export async function deleteUser(id: number) {
+//     const client = await APIClient();
+//
+//     return client.UsersController_remove(id);
+// }
+
 export async function getReviewers() {
     const client = await APIClient();
 
