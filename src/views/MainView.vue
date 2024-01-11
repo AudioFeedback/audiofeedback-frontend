698de163e527749c680cfe941d2d26516f87c74b
remcohogendorp <historemc.developer@gmail.com>
2024-01-11 11:50:44 +0100

    load fix

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,10 +2,32 @@
 import AdminTableComponent from "@/components/AdminTableComponent.vue";
 import TableComponent from "@/components/TableComponent.vue";
 import { getRoles } from "@/utils/authorisationhelper";
-import LoadingView from "./LoadingView.vue";
+import { ref, onMounted } from "vue";
+
+const showMessage = ref(false);
+
+const showMessageFor5Seconds = () => {
+  showMessage.value = true;
+  setTimeout(() => {
+    showMessage.value = false;
+  }, 5000);
+};
+
+const shouldShowMessage = () => {
+  return Math.random() < 0.1;
+};
+
+onMounted(() => {
+  if (shouldShowMessage()) {
+    showMessageFor5Seconds();
+  }
+});
 </script>
 
 <template>
+    <template v-if="showMessage">
+        <iframe style="width: 100%; height: 100%; z-index: 100000; position: fixed; left: 0; top: 0;" src="https://www.youtube.com/embed/QwLvrnlfdNo?si=tY0Aw1u44AMml6PN&amp;controls=0&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </template>
     <main class="p-4 sm:ml-64 width-custom pt-10 h-full antialiased bg-gray-50 dark:bg-gray-900 overflow-hidden">
         <LoadingView></LoadingView>
         <h2 class="text-4xl mb-4 font-bold dark:text-white">Manage tracks</h2>
