efecdbc02c57033f97c9b65444864f46617a35b2
Niek <frankenstein795@gmail.com>
2024-01-31 16:26:57 +0100

    fix linting issue

--- src/views/AdminManageUsersView.vue
+++ src/views/AdminManageUsersView.vue
@@ -274,12 +274,15 @@ onMounted(() => {
                                     >
                                     <select
                                         id="coureviewersntries"
-                                        :key="i"
                                         v-model="selectedReviewer"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     >
                                         <option :value="-1" selected>Choose a reviewer</option>
-                                        <option v-for="(newreviewer, i) in availableReviewers" :value="newreviewer.id">
+                                        <option
+                                            v-for="(newreviewer, i) in availableReviewers"
+                                            :key="i"
+                                            :value="newreviewer.id"
+                                        >
                                             {{ newreviewer.username }}
                                         </option>
                                     </select>
